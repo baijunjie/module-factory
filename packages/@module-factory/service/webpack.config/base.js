e36fe768437f09cb94c105072726581d1d134fe3
baijunjie <bjj19851010@gmail.com>
2019-10-25 18:36:23 +0900

    v0.1.17

--- packages/@module-factory/service/webpack.config/base.js
+++ packages/@module-factory/service/webpack.config/base.js
@@ -8,6 +8,8 @@ module.exports = merge({
     filename: '[name].js',
     library: '[name]',
     libraryTarget: 'umd',
+    libraryExport: 'default',
+    umdNamedDefine: true,
     globalObject: 'this'
   },
   module: {
@@ -24,11 +26,6 @@ module.exports = merge({
 
           // 这个插件可以兼容一些ES6新增特性，但是也会增加代码体积，慎用
           // '@babel/plugin-transform-runtime',
-
-          // 这个插件的作用是
-          // export default 导出的 ES6 模块被 babel 转义成 UMD 模块后
-          // require 该模块可以直接得到模块返回值，而不是在 default 属性上
-          'add-module-exports'
         ]
       }
     }]
